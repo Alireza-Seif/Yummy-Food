8d0c9275032fb2ca2864650be25cc66b87786725
alirezaseif20 <alirezaariye@gmail.com>
2024-03-07 22:31:05 +0330

    feat: add App Colors

---
+++ lib/constants/theme/app_colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  // Primary Colors
+  static const primary = Color(0xFFF87146);
+  static const secondary = Color(0xFF332C45);
+
+  static const error = Color(0xFFE53935);
+  
+  static const success = Color(0xFF3D843C);
+
+  static const warning = Color(0xFFFACC15);
+
+  static const dark = Color(0xFF000000);
+  
+  static const beige = Color(0xFFFFF9EB);
+  
+  static const grey = Color(0xFFF2F2F2);
+
+  static const grayscale10 = Color(0xFFFDFDFD);
+  static const grayscale20 = Color(0xFFECF1F6);
+  static const grayscale30 = Color(0xFFE3E9ED);
+  static const grayscale40 = Color(0xFFD1D8DD);
+  static const grayscale50 = Color(0xFFBFC6CC);
+  static const grayscale60 = Color(0xFF9CA4AB);
+  static const grayscale70 = Color(0xFF78828A);
+  static const grayscale80 = Color(0xFF66707A);
+  static const grayscale90 = Color(0xFF434E58);
+  static const grayscale100 = Color(0xFF171725);
+
+  const AppColors._();
+}
