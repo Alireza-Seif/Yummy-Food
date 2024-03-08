13c5c86522bb926cab9c8e631f32a41d933a06fe
alirezaseif20 <alirezaariye@gmail.com>
2024-03-09 01:31:30 +0330

    feat: add flutter_svg package & icon_view

---
+++ lib/constants/theme/app_icons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class AppIcons{
+  static const _path= 'assets/icons';
+
+  //auth icons
+  static const google = '$_path/Google.svg';
+  
+
+  const AppIcons._();
+}
