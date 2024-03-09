a529ede4a72133953f6c628e2e7d32a78603a140
alirezaseif20 <alirezaariye@gmail.com>
2024-03-10 00:15:20 +0330

    feat: add SignUpLink

---
+++ lib/components/rich_text/signup_link.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:yummy_food/constants/theme/app_theme.dart';
+import 'package:yummy_food/screens/auth/signup_screen.dart';
+
+class SignUpLink extends StatelessWidget {
+  const SignUpLink({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+       RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Do not have an account? ',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'SF Pro Display'),
+            ),
+            TextSpan(
+                text: 'Sign up',
+                style: MyTextStyles.text_button_one,
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignUpScreen(),
+                      ),
+                    );
+                  }),
+          ],
+        ),
+       );
+  }
+}
