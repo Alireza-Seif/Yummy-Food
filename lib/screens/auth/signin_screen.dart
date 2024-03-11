b3da0e2d6f570329da91ed86dc5238bda5a6a663
alirezaseif20 <alirezaariye@gmail.com>
2024-03-12 00:13:40 +0330

    feat: add Sign in screen

---
+++ lib/screens/auth/signin_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:yummy_food/components/buttons/apple_btn.dart';
+import 'package:yummy_food/components/buttons/face_book_btn.dart';
+import 'package:yummy_food/components/buttons/google_btn.dart';
+import 'package:yummy_food/components/buttons/signin_btn.dart';
+import 'package:yummy_food/components/or_line.dart';
+import 'package:yummy_food/components/rich_text/signup_link.dart';
+import 'package:yummy_food/components/text_field/password_tf.dart';
+import 'package:yummy_food/components/text_field/phone_number_tf.dart';
+import 'package:yummy_food/constants/theme/app_theme.dart';
+import 'package:yummy_food/widgets/auth/appbar/appbar_x.dart';
+import 'package:yummy_food/screens/home/home_screen.dart';
+import 'package:yummy_food/screens/splash/on_boarding_screen.dart';
+import 'package:yummy_food/widgets/auth/forgot_tb.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            AppBarX(title: 'Sign in',destinationPage: Onboarding()),
+            SizedBox(height: 24,),
+            PhoneNumberTF(),
+            SizedBox(height: 16,),
+            PasswordTF(),
+            ForgotTB(),
+            SizedBox(height: 32.0),
+            SignInBTN(
+              destinationPage: HomeScreen(),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            OrLine(),
+            SizedBox(
+              height: 24,
+            ),
+            GoogleBTN(),
+            SizedBox(
+              height: 8,
+            ),
+            FaceBookBTN(),
+            SizedBox(
+              height: 8,
+            ),
+            AppleBTN(),
+            SizedBox(
+              height: 26,
+            ),
+            SignUpLink(),
+            SizedBox(
+              height: 75,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
