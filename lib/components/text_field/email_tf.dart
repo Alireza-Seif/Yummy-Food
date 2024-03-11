f3c5e5df1c358675dfd27329ef49d092617f5f28
alirezaseif20 <alirezaariye@gmail.com>
2024-03-12 00:43:15 +0330

    feat: add EmailTf & FullNameTF

---
+++ lib/components/text_field/email_tf.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:yummy_food/constants/theme/app_colors.dart';
+import 'package:yummy_food/constants/theme/app_theme.dart';
+
+class EmailTF extends StatefulWidget {
+  const EmailTF({Key? key}) : super(key: key);
+
+  @override
+  _PhoneTextFieldState createState() => _PhoneTextFieldState();
+}
+
+class _PhoneTextFieldState extends State<EmailTF> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            SizedBox(
+              width: 25,
+            ),
+            Text(
+              'Email',
+              style: MyTextStyles.title_style_two,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        SizedBox(
+          width: 360,
+          height: 50,
+          child: TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              hintText: 'Enter email',
+              hintStyle: MyTextStyles.input_style_one,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: AppColors.grayscale60)),
+            ),
+            keyboardType: TextInputType.number,
+            textInputAction: TextInputAction.done,
+          ),
+        ),
+      ],
+    );
+  }
+}
