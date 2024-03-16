f62161908979455803081886a2b76a33aea5d28d
alirezaseif20 <alirezaariye@gmail.com>
2024-03-16 17:26:15 +0330

    feat: add some icons for drawer

--- lib/constants/theme/app_icons.dart
+++ lib/constants/theme/app_icons.dart
@@ -8,6 +8,11 @@ class AppIcons{
   static const google = '$_path/Google.svg';
   static const apple = '$_path/Apple.svg';
   static const face_book = '$_path/Facebook.svg';
+
+  //Drawer
+  static const wallet = '$_path/wallet.svg';
+  static const location = '$_path/location.svg';
+  static const discount_shape = '$_path/discount_shape.svg';
   
 
   const AppIcons._();
