0f0f01dc671248697626e8cf61b3b3fb2b3627f4
alirezaseif20 <alirezaariye@gmail.com>
2024-03-30 20:28:56 +0330

    feat: change category wrap icons to webp

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -2,8 +2,19 @@ class MyImages {
   static String yummy = 'assets/images/yummy_food.png';
   static String onBoarding = 'assets/images/onboarding.png';
 
-  // banner
+  //Home Banner
   static String slide1 = 'assets/images/slider/slide1.png';
   static String slide2 = 'assets/images/slider/slide2.webp';
   static String slide3 = 'assets/images/slider/slide3.png';
+
+  //Home Wrap
+  static String flash_dals = 'assets/images/category/flash_dals.webp';
+  static String nice_shop = 'assets/images/category/nice_shop.webp';
+  static String points = 'assets/images/category/points.webp';
+  static String rice = 'assets/images/category/rice.webp';
+  static String noodles = 'assets/images/category/noodles.webp';
+  static String vegetable = 'assets/images/category/vegetable.webp';
+  static String bbq = 'assets/images/category/bbq.webp';
+  static String other = 'assets/images/category/other.webp';
+  
 }
