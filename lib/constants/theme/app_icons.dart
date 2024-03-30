4d71308f56a77f7364c03211aeb7188d55cbb3f7
alirezaseif20 <alirezaariye@gmail.com>
2024-03-30 20:05:28 +0330

    feat: add icons for category wrap

--- lib/constants/theme/app_icons.dart
+++ lib/constants/theme/app_icons.dart
@@ -18,7 +18,17 @@ class AppIcons{
   static const notification = '$_path/notification.svg';
   static const bag = '$_path/bag.svg';
   static const search_normal = '$_path/search_normal.svg';
-  
+
+  //Home Wrap
+  static const flash_deals = '$_path/flash_deals.svg';
+  static const nice_shop = '$_path/nice_shop.svg';
+  static const points = '$_path/points.svg';
+  static const rice = '$_path/rice.svg';
+  static const noodles = '$_path/noodles.svg';
+  static const vegetable = '$_path/vegetable.svg';
+  static const bbq = '$_path/bbq.svg';
+  static const other = '$_path/other.svg';
+
 
   const AppIcons._();
 }
